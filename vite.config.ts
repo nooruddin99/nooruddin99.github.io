ac2329f47804246e395704af2b9543e63a4e8d2a
Noor ud Din <140816790+nooruddin99@users.noreply.github.com>
2024-08-22 15:12:38 +0500

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nooruddin99.github.io/"
+  // base: "/nooruddin99.github.io/"
+  base: "./"
 })
